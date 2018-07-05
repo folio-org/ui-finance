5b671e3f8a76d5df523dcd7e83e94515d0df6a1b
arvind <bonandrion@gmail.com>
2018-07-05 13:26:13 -0600

    added fiscal year filters

--- src/components/FiscalYear/FiscalYear.js
+++ src/components/FiscalYear/FiscalYear.js
@@ -7,6 +7,7 @@ import transitionToParams from '@folio/stripes-components/util/transitionToParam
 import removeQueryParam from '@folio/stripes-components/util/removeQueryParam';
 import packageInfo from '../../../package';
 import Tabs from '../Tabs';
+import { Filters, SearchableIndexes } from './fiscalYearFilterConfig';
 // Components and Pages
 import css from './css/FiscalYear.css';
 import FiscalYearPane from './FiscalYearPane';
@@ -14,7 +15,8 @@ import FiscalYearView from './FiscalYearView';
 
 const INITIAL_RESULT_COUNT = 30;
 const RESULT_COUNT_INCREMENT = 30;
-const filterConfig = [];
+const filterConfig = Filters();
+const searchableIndexes = SearchableIndexes;
 
 class FiscalYear extends Component {
   static propTypes = {
@@ -179,6 +181,10 @@ class FiscalYear extends Component {
           parentMutator={mutator}
           detailProps={{ stripes }}
           onComponentWillUnmount={onComponentWillUnmount}
+          searchableIndexes={searchableIndexes}
+          selectedIndex={_.get(this.props.resources.query, 'qindex')}
+          searchableIndexesPlaceholder={null}
+          onChangeIndex={this.onChangeIndex}
         />
       </div>
     );

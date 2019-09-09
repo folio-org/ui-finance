319462b6a661f036428a2b37b40512980511847d
Andrei_Shumski2 <Andrei_Shumski2@epam.com>
2019-09-09 10:02:28 +0300

    UIF-91 Transfer fields case insensitive

--- src/common/const/api.js
+++ src/common/const/api.js
@@ -1,7 +1,7 @@
 export const ACQUISITIONS_UNITS_API = 'acquisitions-units/units';
 export const BUDGETS_API = 'finance-storage/budgets';
 export const FISCAL_YEARS_API = 'finance-storage/fiscal-years';
-export const FUND_TYPES_API = 'finance/fund-types';
+export const FUND_TYPES_API = 'finance-storage/fund-types';
 export const FUNDS_API = 'finance-storage/funds';
 export const GROUPS_API = 'finance-storage/groups';
 export const LEDGERS_API = 'finance-storage/ledgers';

449f2d6584bb63d1aa575407e6c92db257798632
Yury Aniskou <klaus24128@gmail.com>
2019-09-19 14:26:46 +0300

    UIF-75 refactoring after review

--- test/bigtest/network/factories/groupFundFiscalYear.js
+++ test/bigtest/network/factories/groupFundFiscalYear.js
@@ -2,8 +2,5 @@ import { Factory, faker } from '@bigtest/mirage';
 
 export default Factory.extend({
   id: faker.random.uuid,
-  ledgerId: faker.random.uuid,
-  fundId: faker.random.uuid,
-  fiscalYearId: faker.random.uuid,
-  available: 1000,
+  available: faker.finance.amount(1000, 10000, 2),
 });

bc8452f9fdaa3e3448f72d6b8e91210e875264b8
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-05-07 12:28:48 +0300

    fix CS

--- src/Transactions/BracketizeTransactionAmount/BracketizeTransactionAmount.js
+++ src/Transactions/BracketizeTransactionAmount/BracketizeTransactionAmount.js
@@ -4,12 +4,11 @@ import PropTypes from 'prop-types';
 import { AmountWithCurrencyField } from '@folio/stripes-acq-components';
 
 export function BracketizeTransactionAmount({ fundId, transaction }) {
-  let amount = transaction.amount;
+  const amount = Math.abs(transaction.amount);
   let showBrackets = false;
 
   if (transaction.toFundId === fundId && transaction.amount < 0) showBrackets = true;
   if (transaction.fromFundId === fundId && transaction.amount > 0) showBrackets = true;
-  amount = Math.abs(transaction.amount);
 
   return (
     <>

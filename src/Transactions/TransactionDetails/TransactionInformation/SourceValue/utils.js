778ec826bbf707ba5702d5434bd9ad16a78ab9e4
Andrei Shumski2 <Andrei_Shumski2@epam.com>
2020-05-20 14:56:52 +0300

    UINV-152 Fix transaction hyperlink to invoice

--- src/Transactions/TransactionDetails/TransactionInformation/SourceValue/utils.js
+++ src/Transactions/TransactionDetails/TransactionInformation/SourceValue/utils.js
@@ -7,9 +7,12 @@ export function getSourceLink({
   sourceInvoiceLineId,
   encumbrance,
 }) {
+  const invoiceLineLink = sourceInvoiceId && sourceInvoiceLineId && `/invoice/view/${sourceInvoiceId}/line/${sourceInvoiceLineId}/view`;
+  const invoiceLink = sourceInvoiceId && `/invoice/view/${sourceInvoiceId}`;
+
   switch (source) {
     case TRANSACTION_SOURCE.invoice:
-      return sourceInvoiceId && sourceInvoiceLineId && `/invoice/view/${sourceInvoiceId}/line/${sourceInvoiceLineId}/view`;
+      return invoiceLineLink || invoiceLink;
     case TRANSACTION_SOURCE.fiscalYear:
       return fiscalYearId && `/finance/fiscalyear/${fiscalYearId}/view`;
     case TRANSACTION_SOURCE.poLine:

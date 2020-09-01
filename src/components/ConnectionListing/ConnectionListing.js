61e6126c60376923135e9665560baea16b228e7c
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-09-01 11:39:28 +0300

    UINV-178 use FrontendSortingMCL (#410)
    * UINV-178 use FrontendSortingMCL

--- src/components/ConnectionListing/ConnectionListing.js
+++ src/components/ConnectionListing/ConnectionListing.js
@@ -1,24 +1,15 @@
-import React, { useMemo, useState, useCallback } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { orderBy } from 'lodash';
 
 import {
-  Icon,
-  MultiColumnList,
-} from '@folio/stripes/components';
-import {
-  AcqEndOfList,
-  acqRowFormatter,
   AmountWithCurrencyField,
-  ASC_DIRECTION,
-  DESC_DIRECTION,
+  FrontendSortingMCL,
 } from '@folio/stripes-acq-components';
 
 const COLUMN_NAME = 'name';
-const defaultColumns = [COLUMN_NAME, 'code', 'allocated', 'unavailable', 'available', 'arrow'];
+const defaultColumns = [COLUMN_NAME, 'code', 'allocated', 'unavailable', 'available'];
 const columnMapping = {
-  arrow: null,
   name: <FormattedMessage id="ui-finance.item.name" />,
   code: <FormattedMessage id="ui-finance.item.code" />,
   allocated: <FormattedMessage id="ui-finance.item.allocated" />,
@@ -26,7 +17,6 @@ const columnMapping = {
   unavailable: <FormattedMessage id="ui-finance.item.unavailable" />,
   available: <FormattedMessage id="ui-finance.item.available" />,
 };
-const alignRowProps = { alignLastColToEnd: true };
 const sorters = {
   [COLUMN_NAME]: ({ name }) => name?.toLowerCase(),
   'code': ({ code }) => code?.toLowerCase(),
@@ -63,38 +53,19 @@ const ConnectionListing = ({ items, currency, openItem, visibleColumns }) => {
         currency={currency || item.currency}
       />
     ),
-    arrow: () => <Icon icon="caret-right" />,
   }), [currency]);
 
-  const [sortedColumn, setSortedColumn] = useState(COLUMN_NAME);
-  const [sortOrder, setSortOrder] = useState(ASC_DIRECTION);
-  const sortedRecords = orderBy(items, sorters[sortedColumn], sortOrder === DESC_DIRECTION ? 'desc' : 'asc');
-  const changeSorting = useCallback((event, { name }) => {
-    if (!sorters[name]) return;
-    if (sortedColumn !== name) {
-      setSortedColumn(name);
-      setSortOrder(DESC_DIRECTION);
-    } else {
-      setSortOrder(sortOrder === DESC_DIRECTION ? ASC_DIRECTION : DESC_DIRECTION);
-    }
-  }, [sortOrder, sortedColumn]);
-
   return (
-    <>
-      <MultiColumnList
-        columnMapping={columnMapping}
-        contentData={sortedRecords}
-        formatter={resultsFormatter}
-        onHeaderClick={changeSorting}
-        onRowClick={openItem}
-        rowFormatter={acqRowFormatter}
-        rowProps={alignRowProps}
-        sortDirection={sortOrder}
-        sortedColumn={sortedColumn}
-        visibleColumns={visibleColumns}
-      />
-      <AcqEndOfList totalCount={items?.length} />
-    </>
+    <FrontendSortingMCL
+      columnMapping={columnMapping}
+      contentData={items}
+      formatter={resultsFormatter}
+      hasArrow
+      onRowClick={openItem}
+      sortedColumn={COLUMN_NAME}
+      sorters={sorters}
+      visibleColumns={visibleColumns}
+    />
   );
 };
 

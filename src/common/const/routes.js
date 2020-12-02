c0175f41f383fed973cde7afda72b582ed00a267
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2020-12-02 15:53:03 +0300

    fix test

--- src/common/const/routes.js
+++ src/common/const/routes.js
@@ -4,6 +4,7 @@ export const FISCAL_YEAR_VIEW_ROUTE = `${FISCAL_YEAR_ROUTE}/view/`;
 export const FISCAL_YEAR_EDIT_ROUTE = `${FISCAL_YEAR_ROUTE}/edit/`;
 export const FUNDS_ROUTE = '/finance/fund';
 export const LEDGERS_ROUTE = '/finance/ledger';
+export const LEDGER_CREATE_ROUTE = `${LEDGERS_ROUTE}/create`;
 export const LEDGER_EDIT_ROUTE = `${LEDGERS_ROUTE}/edit/`;
 export const BUDGET_ROUTE = '/finance/budget/';
 export const BUDGET_VIEW_ROUTE = '/view/';

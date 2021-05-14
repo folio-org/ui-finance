aa7b8be8db047d6ee69d5cafbbcb489218209e99
Andrei Shumski <Andrei_Shumski2@epam.com>
2021-05-14 14:39:51 +0300

    UIF-305 Implement Keyboard shortcuts modal

--- src/index.js
+++ src/index.js
@@ -5,63 +5,111 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
 
+import { AppContextMenu } from '@folio/stripes/core';
 import {
   checkScope,
   CommandList,
   defaultKeyboardShortcuts,
   HasCommand,
+  KeyboardShortcutsModal,
+  NavList,
+  NavListItem,
+  NavListSection,
 } from '@folio/stripes/components';
+
+import {
+  acqCommands,
+  useModalToggle,
+} from '@folio/stripes-acq-components';
 
 import Main from './components/Main';
 import Settings from './settings';
 
-class Finance extends React.Component {
-  static propTypes = {
-    match: PropTypes.object,
-    showSettings: PropTypes.bool,
-    stripes: PropTypes.object,
-    history: PropTypes.object,
-  }
-
-  focusSearchField = () => {
+const Finance = ({
+  match,
+  showSettings,
+  location,
+}) => {
+  const [isOpen, toggleModal] = useModalToggle();
+  const focusSearchField = () => {
     const el = document.getElementById('input-record-search');
 
     if (el) {
       el.focus();
     }
-  }
+  };
 
-  shortcuts = [
+  const shortcuts = [
     {
       name: 'search',
-      handler: this.focusSearchField,
+      handler: focusSearchField,
+    },
+    {
+      name: 'openShortcutModal',
+      shortcut: 'mod+alt+k',
+      handler: toggleModal,
     },
   ];
 
-  render() {
-    if (this.props.showSettings) {
-      return <Settings {...this.props} />;
-    }
-
+  if (showSettings) {
     return (
+      <Settings
+        match={match}
+        location={location}
+      />
+    );
+  }
+
+  return (
+    <>
       <CommandList commands={defaultKeyboardShortcuts}>
         <HasCommand
-          commands={this.shortcuts}
+          commands={shortcuts}
           isWithinScope={checkScope}
           scope={document.body}
         >
+          <AppContextMenu>
+            {handleToggle => (
+              <NavList>
+                <NavListSection>
+                  <NavListItem
+                    id="keyboard-shortcuts-item"
+                    onClick={() => {
+                      handleToggle();
+                      toggleModal();
+                    }}
+                  >
+                    <FormattedMessage id="stripes-acq-components.appMenu.keyboardShortcuts" />
+                  </NavListItem>
+                </NavListSection>
+              </NavList>
+            )}
+          </AppContextMenu>
           <Switch>
             <Route
-              path={`${this.props.match.path}`}
-              render={() => <Main {...this.props} />}
+              path={`${match.path}`}
+              render={() => <Main match={match} />}
             />
-            <Route component={() => { this.NoMatch(); }} />
           </Switch>
         </HasCommand>
       </CommandList>
-    );
-  }
-}
+      {isOpen && (
+        <KeyboardShortcutsModal
+          open
+          onClose={toggleModal}
+          allCommands={acqCommands}
+        />
+      )}
+    </>
+  );
+};
+
+Finance.propTypes = {
+  match: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+  showSettings: PropTypes.bool,
+};
 
 export default Finance;

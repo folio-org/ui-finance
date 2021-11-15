1be86ecc5adf00f3e0571b37d0dae39dd33b2551
Andrei Shumski <Andrei_Shumski2@epam.com>
2021-11-15 10:21:27 +0300

    UIF-342 Implement MCL Next/Previous pagination (#587)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## (IN PROGRESS)
 
+* Transactions - Implement MCL Next/Previous pagination. Refs UIF-342.
+
 ## [3.0.1](https://github.com/folio-org/ui-finance/tree/v3.0.1) (2021-11-02)
 [Full Changelog](https://github.com/folio-org/ui-finance/compare/v3.0.0...v3.0.1)
 
---
+++ src/Transactions/TransactionsList/hooks/useTransactions/useTransactions.js
@@ -0,0 +1,46 @@
+import { useQuery } from 'react-query';
+import { useLocation } from 'react-router';
+import queryString from 'query-string';
+
+import {
+  useNamespace,
+  useOkapiKy,
+} from '@folio/stripes/core';
+
+import { TRANSACTIONS_API } from '../../../../common/const';
+import { useBuildQuery } from '../useBuildQuery';
+
+export const useTransactions = ({ budget, pagination }) => {
+  const ky = useOkapiKy();
+  const [namespace] = useNamespace({ key: 'transactions-list' });
+
+  const { search } = useLocation();
+  const queryParams = queryString.parse(search);
+  const buildQuery = useBuildQuery();
+  const query = buildQuery(queryParams, budget);
+
+  const searchParams = {
+    query,
+    limit: pagination.limit,
+    offset: pagination.offset,
+  };
+
+  const queryKey = [namespace, pagination.timestamp, pagination.limit, pagination.offset];
+  const queryFn = () => ky.get(TRANSACTIONS_API, { searchParams }).json();
+
+  const options = {
+    enabled: Boolean(budget?.id) && Boolean(pagination.timestamp),
+    keepPreviousData: true,
+  };
+
+  const { isFetching, data } = useQuery(
+    queryKey,
+    queryFn,
+    options,
+  );
+
+  return ({
+    ...data,
+    isFetching,
+  });
+};

fc7e4f4b336788f16f3e5a6851d02ca36bb2a36e
Yury Saukou <yury_saukou@epam.com>
2022-01-13 14:08:49 +0300

    UIF-316 Display transactions as Voided for canceled invoices (#611)

--- src/Transactions/BracketizeTransactionAmount/BracketizeTransactionAmount.js
+++ src/Transactions/BracketizeTransactionAmount/BracketizeTransactionAmount.js
@@ -1,28 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 
+import { IconButton, InfoPopover } from '@folio/stripes/components';
 import { AmountWithCurrencyField } from '@folio/stripes-acq-components';
 
-export function BracketizeTransactionAmount({ fundId, transaction }) {
+import css from './BracketizeTransactionAmount.css';
+
+export function BracketizeTransactionAmount({
+  fundId,
+  transaction,
+  iconSize,
+}) {
   const amount = Math.abs(transaction.amount);
+  const isVoided = transaction.invoiceCancelled;
   let showBrackets = false;
 
   if (transaction.toFundId === fundId && transaction.amount < 0) showBrackets = true;
   if (transaction.fromFundId === fundId && transaction.amount > 0) showBrackets = true;
 
+  // eslint-disable-next-line react/prop-types
+  const renderTrigger = ({ open, ref, toggle }) => (
+    <IconButton
+      ref={ref}
+      icon="info"
+      innerClassName={css.icon}
+      iconClassName={open ? css.open : ''}
+      iconSize={iconSize}
+      onClick={e => {
+        e.stopPropagation();
+        toggle();
+      }}
+    />
+  );
+
   return (
-    <>
-      {showBrackets && '('}
-      <AmountWithCurrencyField
-        amount={amount}
-        currency={transaction.currency}
-      />
-      {showBrackets && ')'}
-    </>
+    <div className={css.flex}>
+      <span className={isVoided ? css.voided : ''}>
+        <AmountWithCurrencyField
+          amount={amount}
+          currency={transaction.currency}
+          showBrackets={showBrackets}
+        />
+      </span>
+      {isVoided && (
+        <div
+          className={css.flex}
+          onFocus={e => e.stopPropagation()}
+        >
+          <InfoPopover
+            renderTrigger={renderTrigger}
+            content={<FormattedMessage id="ui-finance.transaction.voided" />}
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
 BracketizeTransactionAmount.propTypes = {
   fundId: PropTypes.string.isRequired,
   transaction: PropTypes.object.isRequired,
+  iconSize: PropTypes.string,
 };
--- src/Transactions/BracketizeTransactionAmount/BracketizeTransactionAmount.test.js
+++ src/Transactions/BracketizeTransactionAmount/BracketizeTransactionAmount.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 
 import { BracketizeTransactionAmount } from './BracketizeTransactionAmount';
 
@@ -26,5 +27,21 @@ describe('BracketizeTransactionAmount component', () => {
     const { getByText } = renderBracketizeTransactionAmount({ ...defaultProps, transaction });
 
     expect(getByText('$10.00')).toBeDefined();
+  });
+
+  it('should display info for voided transaction', () => {
+    const transaction = {
+      toFundId: defaultProps.fundId,
+      amount: -10,
+      invoiceCancelled: true,
+    };
+
+    renderBracketizeTransactionAmount({ ...defaultProps, transaction });
+
+    const iconBtn = screen.getByRole('button');
+
+    user.click(iconBtn);
+
+    expect(screen.getByText('ui-finance.transaction.voided')).toBeInTheDocument();
   });
 });

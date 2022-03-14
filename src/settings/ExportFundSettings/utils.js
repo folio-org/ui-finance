f669b8cc3876c1f0440624e9846c0e965170e84d
Mikita Siadykh <mikita_siadykh@epam.com>
2022-03-14 14:00:14 +0400

    UIF-364 exportCsv is deprecated in stripes-utils (#625)

--- src/settings/ExportFundSettings/utils.js
+++ src/settings/ExportFundSettings/utils.js
@@ -1,11 +1,11 @@
 import moment from 'moment';
-import { exportCsv } from '@folio/stripes/util';
+import { exportToCsv } from '@folio/stripes/components';
 import { EXPORT_FUND_FIELDS } from './constants';
 
 export const exportCsvFunds = (fiscalYearCode, fundCodeVsExpClassesTypes = []) => {
   const filename = `fund-codes-export-${fiscalYearCode}-${moment().format('YYYY-MM-DD-hh:mm')}`;
 
-  return exportCsv(
+  return exportToCsv(
     [EXPORT_FUND_FIELDS, ...fundCodeVsExpClassesTypes],
     {
       header: false,

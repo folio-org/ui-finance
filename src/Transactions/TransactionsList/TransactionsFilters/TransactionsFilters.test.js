03f84b521350685734e6fb43c67f03f9486105a6
Yury Saukou <yury_saukou@epam.com>
2022-06-06 13:30:05 +0400

    UIF-383 Filter transactions by source POL or Vendor invoice number

--- src/Transactions/TransactionsList/TransactionsFilters/TransactionsFilters.test.js
+++ src/Transactions/TransactionsList/TransactionsFilters/TransactionsFilters.test.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+
+import { useOkapiKy } from '@folio/stripes/core';
 
 import TransactionsFilters from './TransactionsFilters';
+
+jest.useFakeTimers('modern');
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: jest.fn(() => ({ search: 'encumbrance.sourcePoLineId=456&sourceInvoiceId=456' })),
+}));
+jest.mock('@folio/stripes-core/src/useOkapiKy', () => jest.fn(() => ({})));
 
 const defaultProps = {
   activeFilters: {},
@@ -16,11 +26,25 @@ const renderTransactionFilters = (props = defaultProps) => (render(
 ));
 
 describe('TransactionsFilters component', () => {
-  it('should display transaction list filters', () => {
-    const { getByText } = renderTransactionFilters();
+  beforeEach(() => {
+    useOkapiKy.mockClear().mockReturnValue({
+      get: jest.fn(() => ({
+        json: () => Promise.resolve({
+          invoices: [{ id: 'invoiceId', vendorInvoiceNo: 'vendorInvoiceNo' }],
+          poLines: [{ id: 'poLineId', poLineNumber: 'poLineNumber' }],
+        }),
+      })),
+    });
+  });
 
-    expect(getByText('ui-finance.transaction.type')).toBeDefined();
-    expect(getByText('ui-finance.transaction.source')).toBeDefined();
-    expect(getByText('ui-finance.transaction.expenseClass')).toBeDefined();
+  it('should display transaction list filters', async () => {
+    await act(async () => {
+      renderTransactionFilters();
+      jest.advanceTimersByTime(1500);
+    });
+
+    expect(screen.getByText('ui-finance.transaction.type')).toBeDefined();
+    expect(screen.getByText('ui-finance.transaction.source')).toBeDefined();
+    expect(screen.getByText('ui-finance.transaction.expenseClass')).toBeDefined();
   });
 });

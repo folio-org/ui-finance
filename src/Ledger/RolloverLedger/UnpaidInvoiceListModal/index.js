9e8fd972d0d3cd4ff886c93a8adefa6278f032e5
Andrei Shumski <Andrei_Shumski2@epam.com>
2022-07-01 14:42:59 +0300

    UIF-378 Alert user of unpaid invoices during rollover (#654)

---
+++ src/Ledger/RolloverLedger/UnpaidInvoiceListModal/index.js
@@ -0,0 +1,1 @@
+export { default as UnpaidInvoiceListModal } from './UnpaidInvoiceListModal';
---
+++ src/Ledger/RolloverLedger/hooks/useUnpaidInvoices/useUnpaidInvoices.test.js
@@ -0,0 +1,67 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { useOkapiKy } from '@folio/stripes/core';
+import {
+  INVOICES_API,
+  VENDORS_API,
+} from '@folio/stripes-acq-components';
+
+import { useUnpaidInvoices } from './useUnpaidInvoices';
+
+jest.mock('@folio/stripes/core', () => ({
+  ...jest.requireActual('@folio/stripes/core'),
+  useNamespace: () => ['namespace'],
+  useOkapiKy: jest.fn(),
+}));
+
+const queryClient = new QueryClient();
+// eslint-disable-next-line react/prop-types
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    {children}
+  </QueryClientProvider>
+);
+
+const vendor = {
+  id: 'vendorId',
+  name: 'Amazon',
+};
+
+const invoices = [{
+  id: 'invoiceId',
+  vendorId: vendor.id,
+}];
+
+const KY_RESPONSE_DATA_MAP = {
+  [INVOICES_API]: {
+    invoices,
+    totalRecords: invoices.length,
+  },
+  [VENDORS_API]: {
+    organizations: [vendor],
+  },
+};
+
+describe('useUnpaidInvoices', () => {
+  beforeEach(() => {
+    useOkapiKy
+      .mockClear()
+      .mockReturnValue({
+        get: (path) => ({
+          json: () => Promise.resolve(KY_RESPONSE_DATA_MAP[path]),
+        }),
+      });
+  });
+
+  it('should return fetched hydrated invoices', async () => {
+    const { result, waitFor } = renderHook(() => useUnpaidInvoices({
+      periodStart: '2021-01-01T00:00:00.000+00:00',
+      periodEnd: '2021-12-31T00:00:00.000+00:00',
+    }), { wrapper });
+
+    await waitFor(() => !result.current.isFetching);
+
+    expect(result.current.invoices).toEqual([{ ...invoices[0], vendor }]);
+  });
+});

f1b94c75b1ada43242ae600c726f8231862334c2
Yury Saukou <yury_saukou@epam.com>
2022-09-21 11:33:48 +0400

    refine error message; update handler

--- src/Ledger/RolloverLedger/hooks/useRolloverErrorHandler/useRolloverErrorHandler.test.js
+++ src/Ledger/RolloverLedger/hooks/useRolloverErrorHandler/useRolloverErrorHandler.test.js
@@ -1,12 +1,22 @@
+import { FormattedMessage } from 'react-intl';
 import { renderHook } from '@testing-library/react-hooks';
 
+import { useOkapiKy } from '@folio/stripes/core';
 import {
   ERROR_CODE_CONFLICT,
   useShowCallout,
 } from '@folio/stripes-acq-components';
 
+import {
+  FISCAL_YEARS_API,
+  LEDGER_ROLLOVER_API,
+} from '../../../../common/const';
 import { useRolloverErrorHandler } from './useRolloverErrorHandler';
 
+jest.mock('@folio/stripes/core', () => ({
+  ...jest.requireActual('@folio/stripes/core'),
+  useOkapiKy: jest.fn(),
+}));
 jest.mock('@folio/stripes-acq-components', () => ({
   ...jest.requireActual('@folio/stripes-acq-components'),
   useShowCallout: jest.fn(),
@@ -23,10 +33,27 @@ const getErrorResponse = (code) => ({
   json: () => Promise.resolve({ errors: [{ code }] }),
 });
 
+const kyResponsecMap = {
+  [LEDGER_ROLLOVER_API]: {
+    ledgerFiscalYearRollovers: [{
+      toFiscalYearId: 'toFiscalYearId',
+    }],
+  },
+  [`${FISCAL_YEARS_API}/toFiscalYearId`]: {
+    code: 'FY2022',
+  },
+};
+
 describe('useRolloverErrorHandler', () => {
   const showCallout = jest.fn();
+  const getKyMock = ({ resolves = true } = {}) => ({
+    get: jest.fn((url) => ({
+      json: () => Promise[resolves ? 'resolve' : 'reject'](kyResponsecMap[url]),
+    })),
+  });
 
   beforeEach(() => {
+    useOkapiKy.mockClear().mockReturnValue(getKyMock());
     useShowCallout.mockClear().mockReturnValue(showCallout);
   });
 
@@ -47,6 +74,18 @@ describe('useRolloverErrorHandler', () => {
 
     expect(showCallout).toHaveBeenCalledWith(expect.objectContaining({
       messageId: 'ui-finance.ledger.rollover.error.conflict',
+    }));
+  });
+
+  it('should use \'Invalid reference\' label if FY fetching was failed', async () => {
+    useOkapiKy.mockClear().mockReturnValue(getKyMock({ resolves: false }));
+
+    const { result } = renderHook(() => useRolloverErrorHandler({ ledger, currentFiscalYear }));
+
+    await result.current(getErrorResponse(ERROR_CODE_CONFLICT));
+
+    expect(showCallout).toHaveBeenCalledWith(expect.objectContaining({
+      values: expect.objectContaining({ toFYcode: <FormattedMessage id="stripes-acq-components.invalidReference" /> }),
     }));
   });
 });

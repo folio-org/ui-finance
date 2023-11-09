0199c0d3f45e72edce966014390d23c18ceecc49
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2023-11-09 19:26:16 +0500

    UIF-473: add list of donors component (#766)
    * UIF-473: add list of donors component
    
    * tests: add unit test
    
    * add: visible columns and column width
    
    * test: fix failing tests
    
    * remove default column and width

--- src/Funds/FundForm/FundForm.test.js
+++ src/Funds/FundForm/FundForm.test.js
@@ -17,6 +17,10 @@ import FundForm from './FundForm';
 jest.mock('@folio/stripes-acq-components/lib/AcqUnits/AcqUnitsField', () => {
   return () => <span>AcqUnitsField</span>;
 });
+jest.mock('@folio/stripes-acq-components', () => ({
+  ...jest.requireActual('@folio/stripes-acq-components'),
+  Donors: jest.fn(() => 'Donors'),
+}));
 jest.mock('@folio/stripes-components/lib/Commander', () => ({
   HasCommand: jest.fn(({ children }) => <div>{children}</div>),
   expandAllSections: jest.fn(),
@@ -67,6 +71,12 @@ describe('FundForm component', () => {
 
     expect(getByText('stripes-acq-components.FormFooter.cancel')).toBeDefined();
     expect(getByText('ui-finance.saveAndClose')).toBeDefined();
+  });
+
+  it('should display Donors', () => {
+    renderFundForm();
+
+    expect(screen.getByText('Donors')).toBeInTheDocument();
   });
 
   describe('Close form', () => {

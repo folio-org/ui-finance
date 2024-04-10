5b4bbbbac0ab33b755921f47c87fc0bd8a8c2762
Yury Saukou <yury_saukou@epam.com>
2024-04-10 13:43:25 +0400

    UIF-518 Apply changes in the fund schema for the locations field

--- src/Funds/FundForm/FundLocations/FundLocationsList/FundLocationsList.js
+++ src/Funds/FundForm/FundLocations/FundLocationsList/FundLocationsList.js
@@ -40,12 +40,14 @@ export const FundLocationsList = ({
 
   const items = useMemo(() => {
     return value
-      .map((locationId) => locations.find(location => locationId === location.id) || {})
+      .map(({ locationId }) => locations.find(location => location.id === locationId) || {})
       .sort((a, b) => a?.name?.localeCompare(b?.name));
   }, [value, locations]);
 
+  const assignedLocationIds = useMemo(() => assignedLocations.map(({ locationId }) => locationId), [assignedLocations]);
+
   const onRemove = useCallback((location) => {
-    const indexToRemove = value.findIndex((locationId) => locationId === location.id);
+    const indexToRemove = value.findIndex(({ locationId }) => locationId === location.id);
 
     if (indexToRemove > -1) {
       remove(indexToRemove);
@@ -66,10 +68,12 @@ export const FundLocationsList = ({
   const removeAll = useCallback(() => removeBatch(range(0, length)), [length, removeBatch]);
 
   const onRecordsSelect = useCallback((records) => {
-    const locationIds = records.map(({ id }) => id);
+    const normalizedLocations = records.map((location) => ({
+      locationId: location.id,
+    }));
 
     removeAll();
-    concat(locationIds);
+    concat(normalizedLocations);
   }, [concat, removeAll]);
 
   const openUnassignModal = (e) => {
@@ -99,7 +103,7 @@ export const FundLocationsList = ({
           id="fund-locations"
           isMultiSelect
           searchLabel={<FormattedMessage id={`${SCOPE_TRANSLATION_ID}.action.add`} />}
-          initialSelected={assignedLocations}
+          initialSelected={assignedLocationIds}
           onRecordsSelect={onRecordsSelect}
         />
 

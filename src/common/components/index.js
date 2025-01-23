a4535b8eb083369c7e291b13145f39794bc5183f
Azizjon Nurov <azizjon_nurov@epam.com>
2025-01-23 21:30:07 +0500

    Fix export error

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -1,3 +1,3 @@
 export { AllocationToolsMenuSection } from './AllocationToolsMenuSection';
 export { DownloadAllocationWorksheetModal } from './DownloadAllocationWorksheetModal';
-export { BatchAllocation } from './BatchAllocation';
+export * from './BatchAllocation';

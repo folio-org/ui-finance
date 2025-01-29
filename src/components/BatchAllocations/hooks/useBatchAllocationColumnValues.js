98796c081f84ffed409644c3ccf0300c6515e016
Azizjon Nurov <azizjon_nurov@epam.com>
2025-01-29 17:03:07 +0500

    Add some css fields

--- src/components/BatchAllocations/hooks/useBatchAllocationColumnValues.js
+++ src/components/BatchAllocations/hooks/useBatchAllocationColumnValues.js
@@ -10,6 +10,7 @@ import { Field } from 'react-final-form';
 import { getFormattedOptions } from '../BatchAllocationsForm/utils';
 import { BARCH_ALLOCATION_STATUS_OPTIONS } from '../constants';
 import { BUDGET_STATUSES_OPTIONS } from '../../Budget/constants';
+import css from '../BatchAllocation.css';
 
 export const useBatchAllocationColumnValues = (intl) => {
   const fundStatusOptions = useMemo(() => getFormattedOptions(intl, BARCH_ALLOCATION_STATUS_OPTIONS), [intl]);
@@ -18,81 +19,99 @@ export const useBatchAllocationColumnValues = (intl) => {
   return {
     fundStatus: ({ rowIndex }) => {
       return (
-        <FieldSelectFinal
-          dataOptions={fundStatusOptions}
-          fullWidth
-          name={`budgetsFunds.${rowIndex}.fundStatus`}
-        />
+        <div className={css.select}>
+          <FieldSelectFinal
+            dataOptions={fundStatusOptions}
+            fullWidth
+            name={`budgetsFunds.${rowIndex}.fundStatus`}
+          />
+        </div>
       );
     },
     budgetStatus: ({ rowIndex }) => {
       return (
-        <FieldSelectFinal
-          dataOptions={budgetStatusOptions}
-          fullWidth
-          name={`budgetsFunds.${rowIndex}.budgetStatus`}
-        />
+        <div className={css.select}>
+          <FieldSelectFinal
+            dataOptions={budgetStatusOptions}
+            fullWidth
+            name={`budgetsFunds.${rowIndex}.budgetStatus`}
+          />
+        </div>
       );
     },
     allocationIncreaseDecrease: ({ rowIndex }) => {
       return (
-        <Field
-          component={TextField}
-          name={`budgetsFunds.${rowIndex}.allocationIncreaseDecrease`}
-          type="number"
-          required
-          placeholder="0.00"
-        />
+        <div className={css.field}>
+          <Field
+            component={TextField}
+            name={`budgetsFunds.${rowIndex}.allocationIncreaseDecrease`}
+            type="number"
+            required
+            placeholder="0.00"
+          />
+        </div>
       );
     },
     totalAllocatedAfter: ({ rowIndex }) => {
       return (
-        <Field
-          component={TextField}
-          name={`budgetsFunds.${rowIndex}.totalAllocatedAfter`}
-          type="number"
-          required
-          placeholder="0.00"
-          disabled
-        />
+        <div className={css.field}>
+          <Field
+            component={TextField}
+            name={`budgetsFunds.${rowIndex}.totalAllocatedAfter`}
+            type="number"
+            required
+            placeholder="0.00"
+            disabled
+          />
+        </div>
       );
     },
     budgetAllowableEncumbrance: ({ rowIndex }) => {
       return (
-        <Field
-          component={TextField}
-          name={`budgetsFunds.${rowIndex}.budgetAllowableEncumbrance`}
-          type="number"
-          required
-          placeholder="0.00"
-        />
+        <div className={css.field}>
+          <Field
+            component={TextField}
+            name={`budgetsFunds.${rowIndex}.budgetAllowableEncumbrance`}
+            type="number"
+            required
+            placeholder="0.00"
+          />
+        </div>
       );
     },
     budgetAllowableExpenditure: ({ rowIndex }) => {
       return (
-        <Field
-          component={TextField}
-          name={`budgetsFunds.${rowIndex}.budgetAllowableExpenditure`}
-          type="number"
-          required
-          placeholder="0.00"
-        />
+        <div className={css.field}>
+          <Field
+            component={TextField}
+            name={`budgetsFunds.${rowIndex}.budgetAllowableExpenditure`}
+            type="number"
+            required
+            placeholder="0.00"
+          />
+        </div>
       );
     },
     transactionDescription: ({ rowIndex }) => {
       return (
-        <Field
-          component={TextField}
-          name={`budgetsFunds.${rowIndex}.transactionDescription`}
-          type="text"
-          required
-          placeholder="Description"
-        />
+        <div className={css.field}>
+          <Field
+            component={TextField}
+            name={`budgetsFunds.${rowIndex}.transactionDescription`}
+            type="text"
+            required
+            placeholder="Description"
+          />
+        </div>
       );
     },
     fundTags: ({ rowIndex }) => {
       return (
-        <FieldTags name={`budgetsFunds.${rowIndex}.fundTags.tagList`} />
+        <div className={css.tags}>
+          <FieldTags
+            labelless
+            name={`budgetsFunds.${rowIndex}.fundTags.tagList`} />
+        </div>
       );
     },
   };

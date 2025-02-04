46fa326b66e2c76f4a0db8770928b50f0c788d3c
Azizjon Nurov <azizjon_nurov@epam.com>
2025-02-04 16:24:29 +0500

    Move query to useBatchAllocation

--- src/components/BatchAllocations/CreateBatchAllocations/CreateBatchAllocations.js
+++ src/components/BatchAllocations/CreateBatchAllocations/CreateBatchAllocations.js
@@ -21,32 +21,32 @@ import {
 } from '../../../common/const';
 import { useFiscalYear } from '../../../common/hooks';
 import { BatchAllocationsForm } from '../BatchAllocationsForm';
-import {
-  BATCH_ALLOCATION_FIELDS,
-  BATCH_ALLOCATION_SORTABLE_FIELDS,
-} from '../constants';
+import { BATCH_ALLOCATION_SORTABLE_FIELDS } from '../constants';
 import { useBatchAllocation, useSourceData } from '../hooks';
 
 export const CreateBatchAllocations = ({ match }) => {
   const history = useHistory();
   const location = useLocation();
-  const { id, fiscalYearId } = match.params;
-  const type = location.pathname.includes(LEDGERS_ROUTE) ? 'ledgerId' : 'groupId';
+  const { id: sourceId, fiscalYearId } = match.params;
+  const sourceType = location.pathname.includes(LEDGERS_ROUTE) ? 'ledgerId' : 'groupId';
   const [
     sortingField,
     sortingDirection,
     changeSorting,
   ] = useLocationSorting(location, history, noop, BATCH_ALLOCATION_SORTABLE_FIELDS);
-  const params = {
-    query: `(fiscalYearId=="${fiscalYearId}" and ${type}=="${id}") 
-            sortby ${sortingField || BATCH_ALLOCATION_FIELDS.fundName}/sort.${sortingDirection || 'ascending'}`,
-  };
-  const { budgetsFunds, isLoading } = useBatchAllocation(params);
 
-  const sourceType = location.pathname.includes(LEDGERS_ROUTE) ?
+  const { budgetsFunds, isLoading } = useBatchAllocation({
+    fiscalYearId,
+    sortingDirection,
+    sortingField,
+    sourceId,
+    sourceType,
+  });
+
+  const source = location.pathname.includes(LEDGERS_ROUTE) ?
     BATCH_ALLOCATIONS_SOURCE.ledger :
     BATCH_ALLOCATIONS_SOURCE.group;
-  const { data } = useSourceData(sourceType, id);
+  const { data } = useSourceData(source, sourceId);
   const { fiscalYear } = useFiscalYear(fiscalYearId);
 
   const save = useCallback(async (formValues) => {
@@ -56,10 +56,10 @@ export const CreateBatchAllocations = ({ match }) => {
 
   const close = useCallback(() => {
     history.push({
-      pathname: `${type === 'ledgerId' ? LEDGERS_ROUTE : GROUPS_ROUTE}/${id}/view`,
+      pathname: `${sourceType === 'ledgerId' ? LEDGERS_ROUTE : GROUPS_ROUTE}/${sourceId}/view`,
       search: location.search,
     });
-  }, [history, location.search, id, type]);
+  }, [history, location.search, sourceId, sourceType]);
 
   const BATCH_EDIT_TITLE = <FormattedMessage id="ui-finance.allocation.batch.form.title.edit" />;
 

6e118859152e2cc15dc1cebaec3f8c0f12a9fc58
Yury Saukou <yury_saukou@epam.com>
2025-02-20 15:42:58 +0400

    unit tests

--- src/Groups/GroupDetails/utils.test.js
+++ src/Groups/GroupDetails/utils.test.js
@@ -1,5 +1,6 @@
 import { LEDGERS_API } from '../../common/const';
 import {
+  filterPreviousGroupFiscalYears,
   getGroupSummary,
   getGroupLedgers,
   getLedgersCurrentFiscalYears,
@@ -74,4 +75,117 @@ test('sortGroupFiscalYears - should sort FYs by periodStart and series', () => {
     fiscalYears[3].series,
     fiscalYears[1].series,
   ]);
+});
+
+describe('filterPreviousGroupFiscalYears', () => {
+  it('should filter out previous fiscal years', () => {
+    expect(
+      filterPreviousGroupFiscalYears(
+        [
+          { id: '1', series: 'FY1', periodEnd: '2022-12-31T00:00:00.000+00:00' },
+          { id: '2', series: 'FY2', periodEnd: '2023-12-31T00:00:00.000+00:00' },
+          { id: '3', series: 'FY1', periodEnd: '2021-12-31T00:00:00.000+00:00' },
+        ],
+        [
+          { id: '4', series: 'FY1', periodStart: '2022-01-01T00:00:00.000+00:00' },
+          { id: '5', series: 'FY2', periodStart: '2024-01-01T00:00:00.000+00:00' },
+        ],
+      ),
+    ).toEqual(
+      [
+        { id: '2', series: 'FY2', periodEnd: '2023-12-31T00:00:00.000+00:00' },
+        { id: '3', series: 'FY1', periodEnd: '2021-12-31T00:00:00.000+00:00' },
+      ],
+    );
+
+    expect(
+      filterPreviousGroupFiscalYears(
+        [
+          { id: '1', series: 'FY1', periodEnd: '2022-12-31T00:00:00.000+00:00' },
+          { id: '2', series: 'FY2', periodEnd: '2023-12-31T00:00:00.000+00:00' },
+          { id: '3', series: 'FY1', periodEnd: '2021-12-31T00:00:00.000+00:00' },
+        ],
+        [
+          { id: '4', series: 'FY1', periodStart: '2021-01-01T00:00:00.000+00:00' },
+        ],
+      ),
+    ).toEqual(
+      [
+        { id: '2', series: 'FY2', periodEnd: '2023-12-31T00:00:00.000+00:00' },
+      ],
+    );
+
+    expect(
+      filterPreviousGroupFiscalYears(
+        [
+          { id: '1', series: 'FY1', periodEnd: '2022-12-31T00:00:00.000+00:00' },
+          { id: '2', series: 'FY2', periodEnd: '2023-12-31T00:00:00.000+00:00' },
+          { id: '3', series: 'FY1', periodEnd: '2021-12-31T00:00:00.000+00:00' },
+        ],
+        [
+          { id: '4', series: 'FY3', periodStart: '2022-01-01T00:00:00.000+00:00' },
+        ],
+      ),
+    ).toEqual(
+      [
+        { id: '1', series: 'FY1', periodEnd: '2022-12-31T00:00:00.000+00:00' },
+        { id: '2', series: 'FY2', periodEnd: '2023-12-31T00:00:00.000+00:00' },
+        { id: '3', series: 'FY1', periodEnd: '2021-12-31T00:00:00.000+00:00' },
+      ],
+    );
+  });
+
+  it('should return empty array if no previous fiscal years', () => {
+    const fiscalYears = [
+      { id: '1', series: 'FY1', periodEnd: '2023-12-31T00:00:00.000+00:00' },
+    ];
+
+    const currentFiscalYears = [
+      { id: '2', series: 'FY1', periodStart: '2023-01-01T00:00:00.000+00:00' },
+    ];
+
+    const filteredFiscalYears = filterPreviousGroupFiscalYears(fiscalYears, currentFiscalYears);
+
+    expect(filteredFiscalYears).toEqual([]);
+  });
+
+  it('should handle empty fiscal years array', () => {
+    const fiscalYears = [];
+
+    const currentFiscalYears = [
+      { id: '1', series: 'FY1', periodStart: '2023-01-01T00:00:00.000+00:00' },
+    ];
+
+    const filteredFiscalYears = filterPreviousGroupFiscalYears(fiscalYears, currentFiscalYears);
+
+    expect(filteredFiscalYears).toEqual([]);
+  });
+
+  it('should handle empty current fiscal years array', () => {
+    const fiscalYears = [
+      { id: '1', series: 'FY1', periodEnd: '2022-12-31T00:00:00.000+00:00' },
+      { id: '2', series: 'FY2', periodEnd: '2023-12-31T00:00:00.000+00:00' },
+    ];
+
+    const currentFiscalYears = [];
+
+    const filteredFiscalYears = filterPreviousGroupFiscalYears(fiscalYears, currentFiscalYears);
+
+    expect(filteredFiscalYears).toEqual(fiscalYears);
+  });
+
+  it('should handle fiscal years with no matching series in current fiscal years', () => {
+    const fiscalYears = [
+      { id: '1', series: 'FY1', periodEnd: '2022-12-31T00:00:00.000+00:00' },
+      { id: '2', series: 'FY2', periodEnd: '2023-12-31T00:00:00.000+00:00' },
+    ];
+
+    const currentFiscalYears = [
+      { id: '3', series: 'FY3', periodStart: '2023-01-01T00:00:00.000+00:00' },
+    ];
+
+    const filteredFiscalYears = filterPreviousGroupFiscalYears(fiscalYears, currentFiscalYears);
+
+    expect(filteredFiscalYears).toEqual(fiscalYears);
+  });
 });

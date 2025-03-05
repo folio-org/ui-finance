bf32102375b89c1814c2ccfbd4f4cae6e9c70b09
Yury Saukou <yury_saukou@epam.com>
2025-03-05 18:17:32 +0400

    UIF-576 Prevent negative total allocations in budgets (#873)
    * UIF-576 Prevent negative total allocations in budgets
    
    * Update PR template
    
    * enhance batch allocation form
    
    * fix upload allocation sorting
    
    * fixes

--- src/components/BatchAllocations/UploadBatchAllocations/buildInitialValues.js
+++ src/components/BatchAllocations/UploadBatchAllocations/buildInitialValues.js
@@ -1,4 +1,4 @@
-import pick from 'lodash/pick';
+import isFinite from 'lodash/isFinite';
 
 import {
   BATCH_ALLOCATION_FIELDS,
@@ -8,7 +8,18 @@ import {
 
 const TAGS_SEPARATOR = ';';
 
+const {
+  budgetAllocationChange: ALLOCATION_CHANGE,
+  budgetAllowableEncumbrance: ALLOWABLE_ENCUMBRANCE,
+  budgetAllowableExpenditure: ALLOWABLE_EXPENDITURE,
+  budgetStatus: BUDGETS_STATUS,
+  fundStatus: FUNDS_STATUS,
+  transactionDescription: TRANSACTION_DESCRIPTION,
+  transactionTag: TRANSACTION_TAGS,
+} = BATCH_ALLOCATION_FIELDS;
+
 const valueOrEmptyString = (value) => (value || '').trim();
+const numberOrInitialValue = (value) => (value ?? isFinite(Number(value)) ? Number(value) : value);
 
 const buildRowKey = (item, fiscalYear) => [
   valueOrEmptyString(fiscalYear.id),
@@ -34,15 +45,22 @@ export const buildInitialValues = (fileData = [], financeData = [], fiscalYear =
         : fileDataMap.get(buildRowKey(item, fiscalYear)) || {};
 
       /* Fields from the CSV file that the form should take into account */
-      const dataItemFields = pick(dataItem, [
-        BATCH_ALLOCATION_FIELDS.fundStatus,
-        BATCH_ALLOCATION_FIELDS.budgetStatus,
-        BATCH_ALLOCATION_FIELDS.budgetAllocationChange,
-        BATCH_ALLOCATION_FIELDS.budgetAllowableEncumbrance,
-        BATCH_ALLOCATION_FIELDS.budgetAllowableExpenditure,
-        BATCH_ALLOCATION_FIELDS.transactionDescription,
-        BATCH_ALLOCATION_FIELDS.transactionTag,
-      ]);
+      const dataItemFields = {
+        [ALLOCATION_CHANGE]: numberOrInitialValue(dataItem[ALLOCATION_CHANGE]),
+        [ALLOWABLE_ENCUMBRANCE]: numberOrInitialValue(dataItem[ALLOWABLE_ENCUMBRANCE]),
+        [ALLOWABLE_EXPENDITURE]: numberOrInitialValue(dataItem[ALLOWABLE_EXPENDITURE]),
+        [BUDGETS_STATUS]: dataItem[BUDGETS_STATUS] || undefined,
+        [FUNDS_STATUS]: dataItem[FUNDS_STATUS] || undefined,
+        [TRANSACTION_DESCRIPTION]: dataItem[TRANSACTION_DESCRIPTION],
+        [TRANSACTION_TAGS]: {
+          tagList: (
+            dataItem[TRANSACTION_TAGS]
+              ?.split(TAGS_SEPARATOR)
+              ?.map((tag) => tag.trim())
+              ?.filter(Boolean)
+          ) || [],
+        },
+      };
 
       return {
         ...BATCH_ALLOCATION_FORM_DEFAULT_FIELD_VALUES,
@@ -50,18 +68,9 @@ export const buildInitialValues = (fileData = [], financeData = [], fiscalYear =
         ...dataItemFields,
         fiscalYearId: fiscalYear.id,
         fiscalYearCode: fiscalYear.code,
-        [BATCH_ALLOCATION_FIELDS.transactionTag]: {
-          tagList: (
-            dataItem[BATCH_ALLOCATION_FIELDS.transactionTag]
-              ?.split(TAGS_SEPARATOR)
-              ?.map((tag) => tag.trim())
-              ?.filter(Boolean)
-          ) || [],
-        },
         [BATCH_ALLOCATION_FORM_SPECIAL_FIELDS._isMissed]: isFundMissedInFile,
       };
-    })
-    .sort((a, b) => a.fundName.localeCompare(b.fundName));
+    });
 
   const invalidFunds = fileData.filter((item) => !actualFundIdsSet.has(item.fundId));
 

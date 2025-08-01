a126e9193e32efaddd0b907d8d674fdaeee18d5f
Yury Saukou <yury_saukou@epam.com>
2025-08-01 15:19:07 +0400

    UIF-617 Display totals for transactions without an expense class in the expense class summary

---
+++ src/common/ExpenseClasses/constants.js
@@ -0,0 +1,11 @@
+export const EXPENSE_CLASS_FIELDS = {
+  awaitingPayment: 'awaitingPayment',
+  encumbered: 'encumbered',
+  expended: 'expended',
+  expenseClassName: 'expenseClassName',
+  expenseClassStatus: 'expenseClassStatus',
+  percentageExpended: 'percentageExpended',
+  status: 'status',
+};
+
+export const UNASSIGNED_ID = 'UNASSIGNED';

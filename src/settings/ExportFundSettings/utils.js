90937d06deabe401cfabfdd77846aae2f39c0032
Yury Saukou <yury_saukou@epam.com>
2025-12-17 12:23:14 +0400

    UIF-597 Replace the moment library with dayjs

--- src/settings/ExportFundSettings/utils.js
+++ src/settings/ExportFundSettings/utils.js
@@ -1,9 +1,12 @@
-import moment from 'moment';
-import { exportToCsv } from '@folio/stripes/components';
+import {
+  dayjs,
+  exportToCsv,
+} from '@folio/stripes/components';
+
 import { EXPORT_FUND_FIELDS } from './constants';
 
 export const exportCsvFunds = (fiscalYearCode, fundCodeVsExpClassesTypes = []) => {
-  const filename = `fund-codes-export-${fiscalYearCode}-${moment().format('YYYY-MM-DD-hh:mm')}`;
+  const filename = `fund-codes-export-${fiscalYearCode}-${dayjs().format('YYYY-MM-DD-hh:mm')}`;
 
   return exportToCsv(
     [EXPORT_FUND_FIELDS, ...fundCodeVsExpClassesTypes],
